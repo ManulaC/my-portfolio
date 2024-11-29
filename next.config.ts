5ff389445bec0b4622ecd5296c403a38e228bf1c
MPcooray <it23194830@my.sliit.lk>
2024-11-30 02:00:27 +0530

    new

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Enables static export
@@ -9,12 +8,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-=======
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
->>>>>>> b75e565e8d53e84ca924c771e7b75f8b79cf8ce4
